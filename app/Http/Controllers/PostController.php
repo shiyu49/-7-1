b6d2c1b5d0b9f3656d7be2f74adba96cd669877e
shiyu49 <s223009@gm.nau.ac.jp>
2024-03-20 15:03:03 +0900

    8-2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,6 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->get()]);
-        //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入
+        return view('posts/index')->with(['posts' => $post->getPaginateBylimit(1)]);
     }
 }
